260f4ab7e10c90bb73ee1fe34213803960bd6d37
Gabriel Bran <bra23590@uvg.edu.gt>
2025-10-09 12:12:02 -0600

    scale from point of origin

--- ship/src/main.rs
+++ ship/src/main.rs
@@ -12,8 +12,11 @@ use std::time::Duration;
 
 use crate::triangle::triangle;
 
-fn transform(vertex: Vector2, translation: Vector2) -> Vector2 {
+fn transform(vertex: Vector2, translation: Vector2, scale: f32) -> Vector2 {
     let mut new_vertex = vertex;
+
+    new_vertex.x *= scale;
+    new_vertex.y *= scale;
     
     new_vertex.x += translation.x;
     new_vertex.y += translation.y;
@@ -21,22 +24,22 @@ fn transform(vertex: Vector2, translation: Vector2) -> Vector2 {
     new_vertex
 }
 
-fn render(framebuffer: &mut Framebuffer, translation: Vector2){
+fn render(framebuffer: &mut Framebuffer, translation: Vector2, scale: f32){
     let start = Vector2::new(0.0, 0.0);
     let end = Vector2::new(300.0, 300.0);
 
-    let tstart = transform(start, translation);
-    let tend = transform(end, translation);
+    let tstart = transform(start, translation, scale);
+    let tend = transform(end, translation, scale);
 
     line::line(framebuffer, tstart, tend);
 
     let v1 = Vector2::new(500.0, 500.0);
-    let v2 = Vector2::new(600.0, 500.0);
+    let v2 = Vector2::new(600.0, 500.0);    
     let v3 = Vector2::new(550.0, 600.0);
 
-    let tv1 = transform(v1, translation);
-    let tv2 = transform(v2, translation);
-    let tv3 = transform(v3, translation);
+    let tv1 = transform(v1, translation, scale);
+    let tv2 = transform(v2, translation, scale);
+    let tv3 = transform(v3, translation, scale);
 
     triangle(framebuffer, tv1, tv2, tv3);
 }
@@ -52,7 +55,8 @@ fn main() {
         .build();
 
     let mut framebuffer = Framebuffer::new(window_width, window_height);
-    let mut translation = Vector2::new(100.0, 0.0);
+    let mut translation = Vector2::new(0.0, 0.0); // traslación original
+    let mut scale = 1.0; // escala original
 
     framebuffer.set_background_color(Color::new(25, 25, 75, 255));
 
@@ -61,19 +65,25 @@ fn main() {
         framebuffer.set_current_color(Color::new(200, 200, 255, 255));
         
         if window.is_key_down(KeyboardKey::KEY_RIGHT) {
-            translation.x += 1.0;
+            translation.x += 10.0;
         }
         if window.is_key_down(KeyboardKey::KEY_LEFT) {
-            translation.x -= 1.0;
+            translation.x -= 10.0;
         }
         if window.is_key_down(KeyboardKey::KEY_UP) {
-            translation.y -= 1.0;
+            translation.y -= 10.0;
         }
         if window.is_key_down(KeyboardKey::KEY_DOWN) {
-            translation.y += 1.0;
+            translation.y += 10.0;
+        }
+        if window.is_key_down(KeyboardKey::KEY_S) {
+            scale += 0.1;
+        }
+        if window.is_key_down(KeyboardKey::KEY_A) {
+            scale -= 0.1;
         }
 
-        render(&mut framebuffer, translation);
+        render(&mut framebuffer, translation, scale);
 
         framebuffer.swap_buffers(&mut window, &raylib_thread);
         
