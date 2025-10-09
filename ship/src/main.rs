ba2cb35af21f2ec9a099dedeba1f9e40cfa6e885
Gabriel Bran <bra23590@uvg.edu.gt>
2025-10-09 12:19:18 -0600

    rotation from point of origin

--- ship/src/main.rs
+++ ship/src/main.rs
@@ -9,27 +9,40 @@ use framebuffer::Framebuffer;
 use raylib::prelude::*;
 use std::thread;
 use std::time::Duration;
+use std::f32::consts::PI;
 
 use crate::triangle::triangle;
 
-fn transform(vertex: Vector2, translation: Vector2, scale: f32) -> Vector2 {
+fn transform(vertex: Vector2, translation: Vector2, scale: f32, rotation: f32) -> Vector2 {
     let mut new_vertex = vertex;
 
+    //rotación
+    let cos_theta = (rotation * PI / 180.0).cos();
+    let sin_theta = (rotation * PI / 180.0).sin();
+    
+    let rotated_x = new_vertex.x * cos_theta - new_vertex.y * sin_theta;
+    let rotated_y = new_vertex.x * sin_theta + new_vertex.y * cos_theta;
+    
+    new_vertex.x = rotated_x;
+    new_vertex.y = rotated_y;
+
+    //escala
     new_vertex.x *= scale;
     new_vertex.y *= scale;
     
+    //traslación
     new_vertex.x += translation.x;
     new_vertex.y += translation.y;
 
     new_vertex
 }
 
-fn render(framebuffer: &mut Framebuffer, translation: Vector2, scale: f32){
+fn render(framebuffer: &mut Framebuffer, translation: Vector2, scale: f32, rotation: f32){
     let start = Vector2::new(0.0, 0.0);
     let end = Vector2::new(300.0, 300.0);
 
-    let tstart = transform(start, translation, scale);
-    let tend = transform(end, translation, scale);
+    let tstart = transform(start, translation, scale, rotation);
+    let tend = transform(end, translation, scale, rotation);
 
     line::line(framebuffer, tstart, tend);
 
@@ -37,9 +50,9 @@ fn render(framebuffer: &mut Framebuffer, translation: Vector2, scale: f32){
     let v2 = Vector2::new(600.0, 500.0);    
     let v3 = Vector2::new(550.0, 600.0);
 
-    let tv1 = transform(v1, translation, scale);
-    let tv2 = transform(v2, translation, scale);
-    let tv3 = transform(v3, translation, scale);
+    let tv1 = transform(v1, translation, scale, rotation);
+    let tv2 = transform(v2, translation, scale, rotation);
+    let tv3 = transform(v3, translation, scale, rotation);
 
     triangle(framebuffer, tv1, tv2, tv3);
 }
@@ -57,6 +70,7 @@ fn main() {
     let mut framebuffer = Framebuffer::new(window_width, window_height);
     let mut translation = Vector2::new(0.0, 0.0); // traslación original
     let mut scale = 1.0; // escala original
+    let mut rotation = 0.0; // rotación original
 
     framebuffer.set_background_color(Color::new(25, 25, 75, 255));
 
@@ -82,8 +96,14 @@ fn main() {
         if window.is_key_down(KeyboardKey::KEY_A) {
             scale -= 0.1;
         }
+        if window.is_key_down(KeyboardKey::KEY_R) {
+            rotation += 10.0;
+        }
+        if window.is_key_down(KeyboardKey::KEY_E) {
+            rotation -= 10.0;
+        }
 
-        render(&mut framebuffer, translation, scale);
+        render(&mut framebuffer, translation, scale, rotation);
 
         framebuffer.swap_buffers(&mut window, &raylib_thread);
         
