5dddb1f86447de194347d766c7cba1409b41734b
Gabriel Bran <bra23590@uvg.edu.gt>
2025-11-03 17:36:21 -0600

    framebuffer change

--- ship/src/framebuffer.rs
+++ ship/src/framebuffer.rs
@@ -29,12 +29,6 @@ impl Framebuffer {
         self.color_buffer.clear_background(self.background_color);
         self.depth_buffer.fill(f32::INFINITY);
     }
-
-    pub fn set_pixel(&mut self, x: i32, y: i32) {
-        if x >= 0 && x < self.width && y >= 0 && y < self.height {
-            self.color_buffer.draw_pixel(x, y, self.current_color);
-        }
-    }
     
     pub fn point(&mut self, x: i32, y: i32, color: Vector3, depth: f32) {
         if x >= 0 && x < self.width && y >= 0 && y < self.height {
